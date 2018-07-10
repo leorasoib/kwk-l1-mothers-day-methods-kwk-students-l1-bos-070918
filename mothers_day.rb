96b3a823facd2482f49781de35998cb8c5b3e141
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-10 17:57:29 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,5 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
-def mothers_day(name="Mom")
+def mothers_day(name)
   puts "Happy Mother's Day, #{name}!I hope you have a great day!"
 end 
 mothers_day("Beyonce")
