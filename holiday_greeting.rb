04fec4135e0c57d23dea07ae54338b1d666029a5
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-10 18:00:19 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,5 +1,5 @@
 def mothers_day(name="Mom",holiday="Mother's Day",from="Your Favorite Child")
   "Happy #{holiday}, #{name}! From #{from}"
 end 
-mothers_day
+puts mothers_day
 mothers_day("Beyonce","Fourth of July","Jay-Z")
